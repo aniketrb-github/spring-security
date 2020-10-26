922803686b17238a234eaccb91e2cf4cd0470827
aniketrb-github <bharsakaleaniket@gmail.com>
2020-10-27 00:17:34 +0530

    add readme for jwt demo

--- spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/SpringSecurityJwtApplication.java
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -21,7 +21,7 @@ public class SpringSecurityJwtApplication {
  * 
  * Below are the 2 sample HTTP POST(LOGIN) and GET(HOME) request
  * 
- curl --location --request POST 'localhost:8080/authenticate' \
+curl --location --request POST 'localhost:8080/authenticate' \
 --header 'Content-Type: application/json' \
 --header 'Cookie: JSESSIONID=13455FC4D060D7C85B0E09B1ADC8E4CB' \
 --data-raw '{
@@ -30,7 +30,7 @@ public class SpringSecurityJwtApplication {
 }'
  
  CURL GET Request
- curl --location --request GET 'localhost:8080/welcome' \
+curl --location --request GET 'localhost:8080/welcome' \
 --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmb29Vc2VySWQiLCJleHAiOjE2MDM3NjUzODIsImlhdCI6MTYwMzcyOTM4Mn0.jb6WqQ27LXniEGakN8s5weCZp5mLDRP8jQqB49wBlZk' \
 --header 'Cookie: JSESSIONID=13455FC4D060D7C85B0E09B1ADC8E4CB' \
 --header 'Content-Type: text/plain' \
